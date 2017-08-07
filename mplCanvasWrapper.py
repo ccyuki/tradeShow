7e12317ef4b8c1be435eeee78fa1ec12adde18bf
Harvey-BONC <ccyuki@qq.com>
2017-08-07 18:01:14 +0800

    dynamic charts can be used

--- mplCanvasWrapper.py
+++ mplCanvasWrapper.py
@@ -10,11 +10,11 @@ import time
 import random
 import threading
 from datetime import datetime
-from matplotlib.dates import  date2num, MinuteLocator, SecondLocator, DateFormatter
+from matplotlib.dates import  date2num, MinuteLocator, SecondLocator, DateFormatter, MicrosecondLocator
+from matplotlib.ticker import MultipleLocator
+import tushare as ts
  
 X_MINUTES = 1 
-Y_MAX = 100 
-Y_MIN = 1 
 INTERVAL = 1
  
 MAXCOUNTER = int(X_MINUTES * 60/ INTERVAL)
@@ -26,30 +26,37 @@ class MplCanvas(FigureCanvas):
         FigureCanvas.__init__(self, self.fig) 
         FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding) 
         FigureCanvas.updateGeometry(self) 
-        self.ax.set_xlabel("time of data generator") 
-        self.ax.set_ylabel('random data value') 
-        self.ax.legend() 
-        self.ax.set_ylim(Y_MIN,Y_MAX) 
-        self.ax.xaxis.set_major_locator(MinuteLocator())  # every minute is a major locator 
-        self.ax.xaxis.set_minor_locator(SecondLocator([10,20,30,40,50])) # every 10 second is a minor locator 
-        self.ax.xaxis.set_major_formatter( DateFormatter('%H:%M:%S') ) #tick label formatter 
+        self.ax.set_title("Get Real Price")
+        self.ax.set_xlabel("time(s)") 
+        self.ax.set_ylabel('price(RMB)') 
+        self.ax.legend(loc='best')  # 显示图例
+        #self.ax.xaxis.set_major_locator(MinuteLocator())  # 时间间隔 every minute is a major locator 
+        self.ax.xaxis.set_major_locator(SecondLocator(interval=3))  # 时间间隔 every second is a major locator 
+        #self.ax.xaxis.set_minor_locator(SecondLocator([10,20,30,40,50])) # every 10 second is a minor locator 
+        self.ax.xaxis.set_major_formatter( DateFormatter('%Y-%m-%d %H:%M:%S') ) #tick label formatter 
+        self.ax.yaxis.set_major_locator(MultipleLocator(10))  # Y轴间隔10
         self.curveObj = None # draw object
-        print ("MplCanvas")
  
-    def plot(self, datax, datay): 
-        print ("plot")
+    def plot(self, datax, datay):
         if self.curveObj is None: 
             #create draw object once 
-            self.curveObj = self.ax.plot_date(np.array(datax), np.array(datay),'bo-')
+            self.curveObj = self.ax.plot_date(np.array(datax), np.array(datay['price']),'bo-', label='price')
+            self.curveObj.extend(self.ax.plot_date(np.array(datax), np.array(datay['a2_p']),'g*-', label='a2_p'))
         else: 
             #update data of draw object 
-            self.curveObj.set_data(np.array(datax), np.array(datay))
-        #update limit of X axis,to make sure it can move 
-        self.ax.set_xlim(datax[0],datax[-1]) 
+            self.curveObj[0].set_data(np.array(datax), np.array(datay['price']))
+            self.curveObj[1].set_data(np.array(datax), np.array(datay['a2_p']))
+            #update limit of X axis,to make sure it can move 
+            if datax[0] != datax[-1]:
+                if len(datax) < 20:
+                    self.ax.set_xlim(datax[0],datax[-1])  # 设置x轴范围
+                else:
+                    self.ax.set_xlim(datax[-20],datax[-1])  # 只显示最后20笔
+        self.ax.set_ylim(min(datay['price'])-10,max(datay['price'])+10)  # list max +/- 10 Y轴范围
         ticklabels = self.ax.xaxis.get_ticklabels() 
         for tick in ticklabels: 
-            tick.set_rotation(25) 
-            self.draw()
+            tick.set_rotation(25)   # x轴标签旋转25°
+        self.draw()
  
 class  MplCanvasWrapper(QtWidgets.QWidget): 
     def __init__(self , parent =None): 
@@ -61,20 +68,17 @@ class  MplCanvasWrapper(QtWidgets.QWidget):
         self.vbl.addWidget(self.canvas) 
         self.setLayout(self.vbl) 
         self.dataX= [] 
-        self.dataY= [] 
+        self.dataY= {'price':[], 'a2_p':[]}
         self.initDataGenerator()
  
     def startPlot(self): 
         self.__generating = True
-        print("canvas start plot")
  
     def pausePlot(self): 
-        self.__generating = False 
-        print("canvas stop plot")
+        self.__generating = False
         pass
  
-    def initDataGenerator(self): 
-        print("canvas initDAtaGenerator")
+    def initDataGenerator(self):
         self.__generating=False 
         self.__exit = False 
         self.tData = threading.Thread(name = "dataGenerator",target = self.generateData) 
@@ -84,25 +88,31 @@ class  MplCanvasWrapper(QtWidgets.QWidget):
         self.__exit  = True 
         self.tData.join()
  
-    def generateData(self): 
-        print ("generateData")
+    def generateData(self):
         counter=0 
         while(True): 
             if self.__exit: 
                 print ("exit.....")
                 break 
-            if self.__generating: 
-                print("generating....")
-                newData = random.randint(Y_MIN, Y_MAX) 
+            if self.__generating:
+                #stockData = ts.get_realtime_quotes('000581')
+                #if stockData is None:
+                #    continue
+                #newDataPrice = stockData.price[0]
+                #newDataA2_p  = stockData.a2_p[0]
+                #newTime = stockData.date[0]+" "+stockData.time[0]
+                newDataPrice = random.randint(Y_MIN, Y_MAX) 
+                newDataA2_p  = random.randint(Y_MIN, Y_MAX) 
                 newTime= date2num(datetime.now()) 
-                print ("newData:%s , newTime:%s" % (newData, newTime))
                 self.dataX.append(newTime) 
-                self.dataY.append(newData) 
+                self.dataY['price'].append(newDataPrice)
+                self.dataY['a2_p'].append(newDataA2_p) 
                 print (self.dataX, self.dataY)
                 self.canvas.plot(self.dataX, self.dataY)                 
                 if counter >= MAXCOUNTER: 
                     self.dataX.pop(0) 
-                    self.dataY.pop(0)
+                    self.dataY['price'].pop(0)
+                    self.dataY['a2_p'].pop(0)
                 else: 
                     counter+=1 
             time.sleep(INTERVAL)
