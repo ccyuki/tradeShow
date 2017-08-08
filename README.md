03020bf7d115bdfc486ab0ea916b750c2ce62971
Harvey-BONC <ccyuki@qq.com>
2017-08-08 11:19:39 +0800

    tow Y ticks

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tradeShow
+    use tushare interface and matplotlib to show some trade info in charts.
+
+# required
+    qypt5
+    matplotlib
+    pandas
+    if you install anaconda, that will convenient 
--- mplCanvasWrapper.py
+++ mplCanvasWrapper.py
@@ -28,20 +28,28 @@ class MplCanvas(FigureCanvas):
         FigureCanvas.updateGeometry(self) 
         self.ax.set_title("Get Real Price")
         self.ax.set_xlabel("time(s)") 
-        self.ax.set_ylabel('price(RMB)') 
-        self.ax.legend(loc='best')  # 显示图例
+        self.ax.set_ylabel('price(RMB)')         
+        #self.ax.legend(loc='best')  # 显示图例
         #self.ax.xaxis.set_major_locator(MinuteLocator())  # 时间间隔 every minute is a major locator 
         self.ax.xaxis.set_major_locator(SecondLocator(interval=3))  # 时间间隔 every second is a major locator 
         #self.ax.xaxis.set_minor_locator(SecondLocator([10,20,30,40,50])) # every 10 second is a minor locator 
         self.ax.xaxis.set_major_formatter( DateFormatter('%Y-%m-%d %H:%M:%S') ) #tick label formatter 
-        self.ax.yaxis.set_major_locator(MultipleLocator(10))  # Y轴间隔10
+        #self.ax.yaxis.set_major_locator(MultipleLocator(10))  # Y轴间隔10
+        self.ax2 = self.ax.twinx()  # 设置双Y轴, 同理twiny()
+        self.ax2.set_ylabel('a2_p(RMB)')
         self.curveObj = None # draw object
+        
+    def getYtick(self, datay):
+        t_avg = sum(datay)/len(datay)
+        t_min = min(datay) - (t_avg-min(datay))
+        t_max = max(datay) + (max(datay)-t_avg)
+        return t_min, t_max
  
     def plot(self, datax, datay):
         if self.curveObj is None: 
             #create draw object once 
             self.curveObj = self.ax.plot_date(np.array(datax), np.array(datay['price']),'bo-', label='price')
-            self.curveObj.extend(self.ax.plot_date(np.array(datax), np.array(datay['a2_p']),'g*-', label='a2_p'))
+            self.curveObj.extend(self.ax2.plot_date(np.array(datax), np.array(datay['a2_p']),'g*-', label='a2_p'))
         else: 
             #update data of draw object 
             self.curveObj[0].set_data(np.array(datax), np.array(datay['price']))
@@ -52,10 +60,14 @@ class MplCanvas(FigureCanvas):
                     self.ax.set_xlim(datax[0],datax[-1])  # 设置x轴范围
                 else:
                     self.ax.set_xlim(datax[-20],datax[-1])  # 只显示最后20笔
-        self.ax.set_ylim(min(datay['price'])-10,max(datay['price'])+10)  # list max +/- 10 Y轴范围
+        minx,maxx = self.getYtick(datay['price'])
+        self.ax.set_ylim(minx,maxx)  # list max +/- 10 Y轴范围
+        minx,maxx = self.getYtick(datay['a2_p'])
+        self.ax2.set_ylim(minx,maxx)
         ticklabels = self.ax.xaxis.get_ticklabels() 
         for tick in ticklabels: 
             tick.set_rotation(25)   # x轴标签旋转25°
+        self.fig.legend([self.curveObj[0], self.curveObj[1]], ['first', 'second'], loc = 'best')  # 多个axes的图例
         self.draw()
  
 class  MplCanvasWrapper(QtWidgets.QWidget): 
@@ -95,15 +107,15 @@ class  MplCanvasWrapper(QtWidgets.QWidget):
                 print ("exit.....")
                 break 
             if self.__generating:
-                #stockData = ts.get_realtime_quotes('000581')
-                #if stockData is None:
-                #    continue
-                #newDataPrice = stockData.price[0]
-                #newDataA2_p  = stockData.a2_p[0]
-                #newTime = stockData.date[0]+" "+stockData.time[0]
-                newDataPrice = random.randint(1, 100) 
-                newDataA2_p  = random.randint(1, 100) 
-                newTime= date2num(datetime.now()) 
+                stockData = ts.get_realtime_quotes('399300')
+                if stockData is None:
+                    continue
+                newDataPrice = float(stockData.price[0])
+                newDataA2_p  = float(stockData.a2_p[0])
+                newTime = stockData.date[0]+" "+stockData.time[0]
+                #newDataPrice = random.randint(1, 100) 
+                #newDataA2_p  = random.randint(1, 100) 
+                #newTime= date2num(datetime.now()) 
                 self.dataX.append(newTime) 
                 self.dataY['price'].append(newDataPrice)
                 self.dataY['a2_p'].append(newDataA2_p) 
